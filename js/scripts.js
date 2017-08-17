06fc6f93bcd0acd0a706de0f3ff7ffa75e736512
John <jonnygovish@gmail.com>
2017-08-17 10:14:36 +0300

    Add javascript adding a function that loops through the saved array using filter to  build a new array from the user input reversing the new array and joining it back to a string create a variable to call the new function on the user input displaying the result in hmtl

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
-$(document).ready(function(){
-  $("#userInfo").submit(function(event){
+//Back-end logic
+function getWordsGreaterThan3(words) {
+  var newArray = words.filter(function(word) {
+    return word.length >= 3
+  });
+  return newArray.reverse().join(" ");
+}
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#userInfo").submit(function(event) {
     event.preventDefault();
-    
+
     // Store user input
-    var sentence = $("#userInput"). val();
-    
+    var sentence = $("#userInput").val();
+
     // Turn user input from string to array
     var sentenceWords = sentence.split(" ");
+    var result = getWordsGreaterThan3(sentenceWords);
 
     // Test
-    alert(sentenceWords);
-    
+    //console.log(getWordsGreaterThan3(sentenceWords));
+    $("#newSentence").text(result);
   });
 });
